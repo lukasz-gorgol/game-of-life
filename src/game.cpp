f0d44f734c2432987cbe0ccca0071b62678f87e1
Lukasz Gorgol <lgorgol@gmail.com>
2024-11-26 01:17:49 +0100

    style - get rid of unused variables

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,6 @@
 #include "../include/engine.hpp"
 #include "../include/config.hpp"
 
-const int GAME_WIDTH = 200;
-const int GAME_HEIGHT = 100;
-
 int main()
 {
     Config configuration { "config.json" };
